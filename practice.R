f8f17ea9e9e0be8d9459f379c54712996aadf6f6
MinaKVN <45792602+MinaKVN@users.noreply.github.com>
2023-07-19 23:57:12 -0400

    adding git hub instruction

--- practice.R
+++ practice.R
@@ -1,10 +1,8 @@
-#GITHUB instructions
+#gitbub instruction
 #library(usethis)
-#?use_github
-#(create_github_token(scopes = c("repo", "user", "gist", "workflow"),description = "DESCRIBE THE TOKEN'S USE CASE", host = NULL)
-#library(usethis)
-#use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
-
+#create_github_token(scopes = c("repo", "user", "gist", "workflow"),description = "DESCRIBE THE TOKEN'S USE CASE",host = NULL)
+#edit_r_environ() #to save the token
+#use_github(protocol = 'https', auth_token = "name of the token")
 # Data manipulation and plotting
 library(dplyr)
 library(ggplot2)
